50c92971033da17562a961042671723c068b0dff
Dev <iwwwi2015@gmail.com>
2024-10-30 14:09:22 -0400

    v1.0.3

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -53,22 +53,25 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     private function initContext(\Illuminate\Foundation\Application $app)
     {
-        $app->instance(DateTimeInterface::class, $app->make(DateTime::class));
-        $app->instance(GetterParentItemCollectionInterface::class, $app->make(GetterParentItemCollection::class));
-        $app->instance(IntegrationVersionRepositoryInterface::class, $app->make(IntegrationVersionRepository::class));
-        $app->instance(IntegrationVersionItemRepositoryInterface::class, $app->make(IntegrationVersionItemRepository::class));
+       try {
+           $app->instance(DateTimeInterface::class, $app->make(DateTime::class));
+           $app->instance(GetterParentItemCollectionInterface::class, $app->make(GetterParentItemCollection::class));
+           $app->instance(IntegrationVersionRepositoryInterface::class, $app->make(IntegrationVersionRepository::class));
+           $app->instance(IntegrationVersionItemRepositoryInterface::class, $app->make(IntegrationVersionItemRepository::class));
 
-        $app->instance(HashGeneratorInterface::class, $app->make(HashGenerator::class));
-        $app->instance(IntegrationVersionManagerInterface::class, $app->make(IntegrationVersionManager::class));
-        $app->instance(IntegrationVersionItemManagerInterface::class, $app->make(IntegrationVersionItemManager::class));
+           $app->instance(HashGeneratorInterface::class, $app->make(HashGenerator::class));
+           $app->instance(IntegrationVersionManagerInterface::class, $app->make(IntegrationVersionManager::class));
+           $app->instance(IntegrationVersionItemManagerInterface::class, $app->make(IntegrationVersionItemManager::class));
 
-        Context::getInstance()
-            ->setDateTime($app->make(DateTimeInterface::class))
-            ->setIntegrationVersionItemRepository($app->make(IntegrationVersionItemRepositoryInterface::class))
-            ->setIntegrationVersionRepository($app->make(IntegrationVersionRepositoryInterface::class))
-            ->setGetterParentItemCollection($app->make(GetterParentItemCollectionInterface::class))
-            ->setIntegrationVersionManager($app->make(IntegrationVersionManagerInterface::class))
-            ->setIntegrationVersionItemManager($app->make(IntegrationVersionItemManagerInterface::class))
-            ->setHashGenerator($app->make(HashGeneratorInterface::class));
+           Context::getInstance()
+               ->setDateTime($app->make(DateTimeInterface::class))
+               ->setIntegrationVersionItemRepository($app->make(IntegrationVersionItemRepositoryInterface::class))
+               ->setIntegrationVersionRepository($app->make(IntegrationVersionRepositoryInterface::class))
+               ->setGetterParentItemCollection($app->make(GetterParentItemCollectionInterface::class))
+               ->setIntegrationVersionManager($app->make(IntegrationVersionManagerInterface::class))
+               ->setIntegrationVersionItemManager($app->make(IntegrationVersionItemManagerInterface::class))
+               ->setHashGenerator($app->make(HashGeneratorInterface::class));
+       } catch (\Exception $e) {
+       }
     }
 }
